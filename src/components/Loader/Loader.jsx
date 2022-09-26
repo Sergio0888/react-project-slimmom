de3757d56a308912c5b48d472d7ffcfc55e36b23
Slava Pravda <pravdaslava7@gmail.com>
2022-09-26 19:24:41 +0300

    add loader && header (exit && name)

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,25 @@
-// Лоадер уже добавлен как пакет, документация ниже
+import React from 'react';
+import { ThreeCircles } from 'react-loader-spinner';
+import s from './Loader.module.scss';
 
-//https://mhnpd.github.io/react-loader-spinner/docs/intro/
+export const Loader = () => {
+  return (
+    <div className={s.loader__block}>
+      <div className={s.loader__wrapper}>
+        <ThreeCircles
+          color="#f07416"
+          arialLabel="loading-indicator"
+          height="120"
+          width="120"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        />
+      </div>
+    </div>
+  );
+};
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,6 +1,8 @@
 export const isLogin = state => state.auth.isLogin;
 
-export const getUserName = state => state.auth.user.username;
+export const getUserName = state => {
+  return state?.auth?.user?.user?.username || state?.auth?.user?.username 
+}
 
 export const getUserId = store => {
   // if(!store.auth.user.user.id) {
